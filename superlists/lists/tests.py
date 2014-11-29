428bc1f90bb29381729aa2fee2750a5ac8d6af82
Georg Dresler <georg@g-dresler.de>
2014-11-29 20:40:59 +0100

    Fixes the indentation.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -8,33 +8,33 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-  def test_root_url_resolves_to_homepage_view(self):
-    found = resolve('/')
-    self.assertEqual(found.func, home_page)
+    def test_root_url_resolves_to_homepage_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
-  def test_home_page_returns_correct_html(self):
-    request = HttpRequest()
-    response = home_page(request)
-    expected_html = render_to_string('home.html')
-    self.assertEqual(response.content.decode(), expected_html)
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
 
-  def test_home_page_can_save_a_POST_request(self):
-    request = HttpRequest()
-    request.method = 'POST'
-    request.POST['item_text'] = 'A new list item'
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
 
-    response = home_page(request)
+        response = home_page(request)
 
-    self.assertEqual(Item.objects.count(), 1)
-    new_item = Item.objects.first()
-    self.assertEqual(new_item.text, 'A new list item')
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
 
-    self.assertIn('A new list item', response.content.decode())
-    expected_html = render_to_string(
-      'home.html',
-      {'new_item_text':  'A new list item'}
-    )
-    self.assertEqual(response.content.decode(), expected_html)
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
 
 
 class ItemModelTest(TestCase):
